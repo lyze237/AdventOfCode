766ee3d2ba5cebb18c1c78fe54b0014a646cbf3e
Lyze <11274700+lyze237@users.noreply.github.com>
2017-12-05 18:24:51 +0100

    added from other pc

---
+++ OLD/Day1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo("input2");
+            string input = File.ReadAllText(file.FullName);
+
+            int sum = 0;
+            var charArray = input.ToCharArray();
+            for (int i = 0; i < charArray.Length / 2; i++)
+            {
+                int left = (int) Char.GetNumericValue(charArray[i]);
+                int right = (int) Char.GetNumericValue(charArray[charArray.Length / 2 + i]);
+
+                if (left == right) {
+                    sum += left;   
+                    System.Console.WriteLine(left + " = " + right);
+                }
+
+            }
+            
+            System.Console.WriteLine("Result: " + (sum * 2));
+        }
+
+        static void Challenge1(string[] args)
+        {
+            FileInfo file = new FileInfo("input");
+            string input = File.ReadAllText(file.FullName);
+
+            int sum = 0;
+            var charArray = input.ToCharArray();
+            for (int i = 1; i < charArray.Length; i++)
+            {
+                int left = (int) Char.GetNumericValue(charArray[i - 1]);
+                int right = (int) Char.GetNumericValue(charArray[i]);
+
+                if (left == right) {
+                    sum += left;   
+                    System.Console.WriteLine(left + " = " + right);
+                }
+
+            }
+
+            int left2 = (int) Char.GetNumericValue(charArray[0]);
+            int right2 = (int) Char.GetNumericValue(charArray[charArray.Length - 1]);
+
+            if (left2 == right2) {
+                sum += left2;
+            }
+
+            System.Console.WriteLine("Result: " + sum);
+        }
+    }
+}
---
+++ OLD/Day2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Challenge1(string[] args)
+        {
+            int sum = 0;
+            FileInfo file = new FileInfo("input");
+            foreach (var line in File.ReadAllLines(file.FullName))
+            {
+                var inputs = line.Split('\t');
+                int min = Int32.MaxValue;
+                int max = Int32.MinValue;
+                foreach (var input in inputs)
+                {
+                    var number = Convert.ToInt32(input);
+
+                    if (number < min)
+                        min = number;
+
+                    if (number > max)
+                        max = number;
+                }
+                sum += max - min;
+            }
+
+            Console.WriteLine("The Sum is: " + sum);
+        }
+        
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            FileInfo file = new FileInfo("input2");
+            foreach (var line in File.ReadAllLines(file.FullName))
+            {
+                var inputs = line.Split('\t');
+                foreach (var inputLeft in inputs)
+                {
+                    var numberLeft = Convert.ToInt32(inputLeft);
+                    
+                    foreach (var inputRight in inputs)
+                    {
+                        var numberRight = Convert.ToInt32(inputRight);
+
+                        int max = Math.Max(numberLeft, numberRight);
+                        int min = Math.Min(numberLeft, numberRight);
+
+                        if (max % min == 0 && max != min)
+                        {
+                            Console.WriteLine(line + $": max {max} / min {min}");
+                            sum += max / min;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("The Sum is: " + sum / 2);
+        }
+    }
+}
