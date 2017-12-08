52abe32cc176a94fe6f09d5db87e65fca1619b3a
Lyze <11274700+lyze237@users.noreply.github.com>
2017-12-08 21:06:18 +0100

    Added day 8

---
+++ Day8/Day8Challenge2.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ using Utils;
+
+namespace Day8
+{
+    public class Day8Challenge2 : Challenge<int>
+    {
+        public Day8Challenge2() : base("input")
+        {
+        }
+
+        public override int Run()
+        {
+            List<Instruction> instructions = new List<Instruction>();
+            Dictionary<string, int> registers = new Dictionary<string, int>();
+            int maxValue = int.MinValue; 
+            Regex regex = new Regex(@"(\w+) (dec|inc) (-?\d+) if (\w+) ([<=>!]{1,2}) (-?\d+)");
+            foreach (string line in GetInputFilePerLine())
+            {
+                Match match = regex.Match(line);
+                instructions.Add(new Instruction(match.Groups[1].Value, match.Groups[4].Value,
+                    int.Parse(match.Groups[3].Value), match.Groups[2].Value.Equals("inc"),
+                    match.Groups[5].Value, int.Parse(match.Groups[6].Value)));
+                registers[match.Groups[1].Value] = 0;
+            }
+
+            foreach (Instruction instruction in instructions)
+            {
+                switch (instruction.Condition)
+                {
+                    case ">":
+                        if (registers[instruction.OnWho] > instruction.Amount)
+                            registers[instruction.Name] =
+                                instruction.IsInc
+                                    ? registers[instruction.Name] + instruction.ChangeAmount
+                                    : registers[instruction.Name] - instruction.ChangeAmount;
+                        break;
+                    case "<":
+                        if (registers[instruction.OnWho] < instruction.Amount)
+                            registers[instruction.Name] =
+                                instruction.IsInc
+                                    ? registers[instruction.Name] + instruction.ChangeAmount
+                                    : registers[instruction.Name] - instruction.ChangeAmount;
+                        break;
+                    case "<=":
+                        if (registers[instruction.OnWho] <= instruction.Amount)
+                            registers[instruction.Name] =
+                                instruction.IsInc
+                                    ? registers[instruction.Name] + instruction.ChangeAmount
+                                    : registers[instruction.Name] - instruction.ChangeAmount;
+                        break;
+                    case ">=":
+                        if (registers[instruction.OnWho] >= instruction.Amount)
+                            registers[instruction.Name] =
+                                instruction.IsInc
+                                    ? registers[instruction.Name] + instruction.ChangeAmount
+                                    : registers[instruction.Name] - instruction.ChangeAmount;
+                        break;
+                    case "==":
+                        if (registers[instruction.OnWho] == instruction.Amount)
+                            registers[instruction.Name] =
+                                instruction.IsInc
+                                    ? registers[instruction.Name] + instruction.ChangeAmount
+                                    : registers[instruction.Name] - instruction.ChangeAmount;
+                        break;
+                    case "!=":
+                        if (registers[instruction.OnWho] != instruction.Amount)
+                            registers[instruction.Name] =
+                                instruction.IsInc
+                                    ? registers[instruction.Name] + instruction.ChangeAmount
+                                    : registers[instruction.Name] - instruction.ChangeAmount;
+                        break;
+                }
+                if (maxValue < registers[instruction.Name])
+                    maxValue = registers[instruction.Name];
+            }
+            return maxValue;
+        }
+    }
+}
---
+++ Day8/Instruction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day8
+{
+    public class Instruction
+    {
+        public string Name { get; set; }
+
+        public string OnWho { get; set; }
+        public int ChangeAmount { get; set; }
+        public bool IsInc { get; set; }
+        public string Condition { get; set; }
+        public int Amount { get; set; }
+
+        public Instruction(string name, string onWho, int changeAmount, bool isInc, string condition, int amount)
+        {
+            Name = name;
+            OnWho = onWho;
+            ChangeAmount = changeAmount;
+            IsInc = isInc;
+            Condition = condition;
+            Amount = amount;
+        }
+    }
+}
