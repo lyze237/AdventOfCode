a24bf1e94da8c366673571dff8f92422d330a51c
Lyze <11274700+lyze237@users.noreply.github.com>
2017-12-14 18:52:23 +0100

    Added day 14, challenge 2

---
+++ Day14/Day14Challenge1.cs
@@ -0,0 +1,18 @@
+using System.Linq;
+using System;
+using Utils;
+
+namespace Day14
+{
+    public class Day14Challenge1 : Challenge<int>
+    {
+        public Day14Challenge1() : base("input")
+        {
+        }
+
+        public override int Run()
+        {
+            return -1;
+        }
+    }
+}
---
+++ Day14/Day14Challenge2.cs
@@ -0,0 +1,130 @@
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using Day10;
+using Utils;
+
+namespace Day14
+{
+    public class Day14Challenge2 : Challenge<int>
+    {
+        public Day14Challenge2() : base("input")
+        {
+        }
+
+        public override int Run()
+        {
+            var inputFile = GetInputFile();
+            int groups = 0;
+            var things = new List<string>();
+            for (int i = 0; i <= 127; i++)
+            {
+                things.Add(inputFile + "-" + i);
+            }
+            List<List<int>> intRows = things.Select(thing =>
+                    KnotHash(thing).Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')))
+                .Select(list => list.SelectMany(s => s.Select(c => Convert.ToInt32("" + c))).ToList()).ToList();
+
+            for (int row = 0; row < intRows.Count; row++)
+            {
+                var intRow = intRows[row];
+                for (int col = 0; col < intRow.Count; col++)
+                {
+                    int i = intRow[col];
+                    if (i == 1)
+                    {
+                        Eliminate(row, col, intRows);
+                        groups++;
+                    }
+                }
+            }
+            return groups;
+        }
+
+        private static void Eliminate(int row, int col, List<List<int>> intRows)
+        {
+            intRows[row][col] = 0;
+            try
+            {
+                if(intRows[row][col + 1] == 1)
+                    Eliminate(row, col + 1, intRows);
+            }
+            catch (Exception){}
+            try
+            {
+                if(intRows[row + 1][col] == 1)
+                    Eliminate(row + 1, col, intRows);
+            }
+            catch (Exception){}
+            try
+            {
+                if(intRows[row][col - 1] == 1)
+                    Eliminate(row, col - 1, intRows);
+            }
+            catch (Exception){}
+            try
+            {
+                if(intRows[row - 1][col] == 1)
+                    Eliminate(row - 1, col, intRows);
+            }
+            catch (Exception){}
+        }
+
+        /// <summary>
+        /// Copied from Day10 Challenge 2
+        /// </summary>
+        /// <param name="input">The string</param>
+        /// <returns>The knot hash</returns>
+        public string KnotHash(string input)
+        {
+            int[] lengths = input.ToCharArray().Select(c => (int) c)
+                .Append(17).Append(31).Append(73).Append(47).Append(23)
+                .ToArray();
+            int[] tmpList = new int[256];
+            for (int i = 0; i < tmpList.Length; i++)
+            {
+                tmpList[i] = i;
+            }
+            WrapArray<int> list = new WrapArray<int>(tmpList);
+
+            int cursor = 0;
+            int skipSize = 0;
+
+            for (int z = 0; z < 64; z++)
+            {
+                for (int i = 0; i < lengths.Length; i++, skipSize++)
+                {
+                    for (int startIndex = cursor; startIndex < cursor + lengths[i] / 2; startIndex++)
+                    {
+                        int temp = list[cursor + lengths[i] - 1 - (startIndex - cursor)];
+                        list[cursor + lengths[i] - 1 - (startIndex - cursor)] = list[startIndex];
+                        list[startIndex] = temp;
+                    }
+
+                    cursor += lengths[i] + skipSize;
+                }
+            }
+
+            List<int> xorList = new List<int>();
+            for (int i = 0; i < list.Length; i += 16)
+            {
+                int[] toXor = list.Array.Skip(i).Take(16).ToArray();
+                int xorResult = toXor[0];
+
+                for (int j = 1; j < toXor.Length; j++)
+                {
+                    xorResult ^= toXor[j];
+                }
+                xorList.Add(xorResult);
+            }
+
+            string ret = "";
+            foreach (int element in xorList)
+            {
+                ret += element.ToString("X").PadLeft(2, '0');
+            }
+                
+            return ret.ToLower();
+        }
+    }
+}
