e4ce697b893408f8116d0d7417684654369d4e42
Lyze <11274700+lyze237@users.noreply.github.com>
2017-12-18 18:25:31 +0100

    blub

--- Day18/Day18Challenge1.cs
+++ Day18/Day18Challenge1.cs
@@ -7,105 +7,58 @@ namespace Day18
 {
     public class Day18Challenge1 : Challenge<int>
     {
-        
         public Day18Challenge1() : base("input")
         {
         }
 
         public override int Run()
         {
+            int frequency = 0;
+
             List<Instruction> instructions = new List<Instruction>();
-            
+
             foreach (var line in GetInputFilePerLine())
             {
                 var strings = line.Split(' ');
-                
-                if (strings.Length > 2)
-                    instructions.Add(new Instruction(strings[0], strings[1], strings[2]));
-                else
-                    instructions.Add(new Instruction(strings[0], strings[1], string.Empty));
+                instructions.Add(new Instruction(strings));
             }
-
-            int fregency = 0;
-            for (var i = 0; i < instructions.Count; i++)
+            
+            for (var i = 0;; i++)
             {
-                var instruction = instructions[i];
-
-                switch (instruction.Type)
+                Instruction instruction = instructions[i];
+                
+                switch (instruction.Command)
                 {
                     case "snd":
-                        fregency = instruction.GetRegisterValue();
+                        frequency = instruction.RegisterValue;
                         break;
                     case "set":
-                        instruction.SetRegisterValue(instruction.GetInstructionValue());
+                        instruction.RegisterValue = instruction.Value;
                         break;
                     case "add":
-                        instruction.SetRegisterValue(instruction.GetRegisterValue() + instruction.GetInstructionValue());
+                        instruction.RegisterValue += instruction.Value;
                         break;
                     case "mul":
-                        instruction.SetRegisterValue(instruction.GetRegisterValue() * instruction.GetInstructionValue());
+                        instruction.RegisterValue *= instruction.Value;
                         break;
                     case "mod":
-                        instruction.SetRegisterValue(instruction.GetRegisterValue() % instruction.GetInstructionValue());
+                        instruction.RegisterValue %= instruction.Value;
                         break;
                     case "rcv":
-                        if (instruction.GetRegisterValue() > 0)
-                            return fregency;
+                        if (instruction.RegisterValue != 0)
+                            return frequency;
                         break;
                     case "jgz":
-                        if (instruction.GetRegisterValue() > 0)
-                            i += instruction.GetInstructionValue() - 1;
+                        if (instruction.RegisterValue > 0)
+                        {
+                            i += instruction.Value - 1;
+
+                            if (i < 0 || i >= instructions.Count)
+                                return -1;
+                        }
                         break;
                 }
             }
-
-            return -1;
-        }
-    }
-
-    public class Instruction
-    {
-        public static Dictionary<string, int> Registers = new Dictionary<string, int>();
-        
-        public string Type { get; }
-        private string register;
-        private string value;
-
-        public Instruction(string type, string register, string value)
-        {
-            Type = type;
-            this.register = register;
-            this.value = value;
-
-            if (!Registers.ContainsKey(register))
-                Registers.Add(register, 0);
-        }
-
-        public int GetRegisterValue()
-        {
-            return Registers[register];
-        }
-
-        public void SetRegisterValue(int v)
-        {
-            Registers[register] = v;
-        }
-
-        public int GetInstructionValue()
-        {
-            try
-            {
-                return Convert.ToInt32(value);
-            }
-            catch (Exception)
-            {
-                return Registers[value];
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(Type)}: {Type}, {nameof(register)}: {register}, {nameof(register)}.Value: {GetRegisterValue()}, {nameof(value)}: {value}";
         }
     }
 }
---
+++ Day18/Instruction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day18
+{
+    public class Instruction
+    {
+        private static Dictionary<string, int> registers = new Dictionary<string, int>();
+        
+        public string Command { get; }
+        public string Register { get; }
+        private string value;
+
+        public int Value
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Value is not set");
+                try
+                {
+                    return Convert.ToInt32(value);
+                }
+                catch (FormatException)
+                {
+                    return registers[value];
+                }
+            }
+        }
+
+        public int RegisterValue
+        {
+            get => registers[Register];
+            set => registers[Register] = value;
+        }
+        
+        public Instruction(string[] strings)
+        {
+            Command = strings[0];
+            Register = strings[1];
+            
+            if (!registers.ContainsKey(Register))
+                registers.Add(Register, 0);
+            
+            if (strings.Length > 2)
+                value = strings[2];    
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Command)}: {Command}, {nameof(Register)}: {Register}, {nameof(value)}: {value}";
+        }
+    }
+}
