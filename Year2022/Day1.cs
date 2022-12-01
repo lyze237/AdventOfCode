a88dd8cff3bd1d884dc6450915319ad240d41cda
Lyze <11274700+lyze237@users.noreply.github.com>
2022-12-01 21:23:22 +0100

    Implemented 2022-1

--- Year2022/Day1.cs
+++ Year2022/Day1.cs
@@ -4,27 +4,16 @@ namespace AdventOfCode.Year2022;
 
 public class Day1 : Day
 {
-    public override object ExecutePart1()
-    {
-        var output = Input
-                .Split("\n\n")
-                .Select(g => g.Split("\n"))
-                .Select(g => g.Sum(Convert.ToInt64))
-                .OrderByDescending(g => g)
-                .ToList();
+    public override object ExecutePart1() => 
+        SortElves().First();
 
-            return output[0];
-    }
-   
-    public override object ExecutePart2()
-    {
-        var output = Input
+    public override object ExecutePart2() => 
+        SortElves().Take(3).Sum();
+
+    private IEnumerable<long> SortElves() =>
+        Input
             .Split("\n\n")
             .Select(g => g.Split("\n"))
             .Select(g => g.Sum(Convert.ToInt64))
-            .OrderByDescending(g => g)
-            .ToList();
-
-        return output[0] + output[1] + output[2];
-    }
+            .OrderByDescending(g => g);
 }
