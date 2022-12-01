0c44e468208a594404af520b301b3ffc79a0d40a
Lyze <11274700+lyze237@users.noreply.github.com>
2022-12-01 19:42:11 +0100

    Init 2022

---
+++ Year2022/Day1.cs
@@ -0,0 +1,30 @@
+﻿using Tidy.AdventOfCode;
+
+namespace AdventOfCode.Year2022;
+
+public class Day1 : Day
+{
+    public override object ExecutePart1()
+    {
+        var output = Input
+                .Split("\n\n")
+                .Select(g => g.Split("\n"))
+                .Select(g => g.Sum(Convert.ToInt64))
+                .OrderByDescending(g => g)
+                .ToList();
+
+            return output[0];
+    }
+   
+    public override object ExecutePart2()
+    {
+        var output = Input
+            .Split("\n\n")
+            .Select(g => g.Split("\n"))
+            .Select(g => g.Sum(Convert.ToInt64))
+            .OrderByDescending(g => g)
+            .ToList();
+
+        return output[0] + output[1] + output[2];
+    }
+}
