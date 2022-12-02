4f2090d41b933ff2f14c7f1b543a6a3a42b203e1
Lyze <11274700+lyze237@users.noreply.github.com>
2022-12-02 16:29:36 +0100

    Implemented 2022-2

---
+++ Year2022/Day2.cs
@@ -0,0 +1,52 @@
+﻿using Tidy.AdventOfCode;
+
+namespace AdventOfCode.Year2022;
+
+public class Day2 : Day.NewLineSplitParsed<string>
+{
+    public override object ExecutePart1()
+    {
+        return Input.Select(line => line.Split(" "))
+            .Select(input => (input[0], input[1]))
+            .Select(match => match switch
+            {
+                ("A", "X") => 1 + 3,
+                ("A", "Y") => 2 + 6,
+                ("A", "Z") => 3 + 0,
+
+                ("B", "X") => 1 + 0,
+                ("B", "Y") => 2 + 3,
+                ("B", "Z") => 3 + 6,
+
+                ("C", "X") => 1 + 6,
+                ("C", "Y") => 2 + 0,
+                ("C", "Z") => 3 + 3,
+
+                _ => throw new ArgumentOutOfRangeException()
+            })
+            .Sum();
+    }
+
+    public override object ExecutePart2()
+    {
+        return Input.Select(line => line.Split(" "))
+            .Select(input => (input[0], input[1]))
+            .Select(match => match switch
+            {
+                ("A", "X") => 3 + 0,
+                ("A", "Y") => 1 + 3,
+                ("A", "Z") => 2 + 6,
+
+                ("B", "X") => 1 + 0,
+                ("B", "Y") => 2 + 3,
+                ("B", "Z") => 3 + 6,
+
+                ("C", "X") => 2 + 0,
+                ("C", "Y") => 3 + 3,
+                ("C", "Z") => 1 + 6,
+
+                _ => throw new ArgumentOutOfRangeException()
+            })
+            .Sum();
+    }
+}
