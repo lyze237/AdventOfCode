54482369eeaa278c87de59329f3f3cbd91e5ba98
Lyze <11274700+lyze237@users.noreply.github.com>
2022-12-04 19:46:39 +0100

    Implemented 2022-4

---
+++ Year2022/Day4.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Year2022.Extensins;
+using Tidy.AdventOfCode;
+
+namespace AdventOfCode.Year2022;
+
+public class Day4 : Day<List<(List<int>, List<int>)>>
+{
+    public override List<(List<int>, List<int>)> ParseInput(string rawInput)
+    {
+        return rawInput.Split("\n")
+            .Select(line => line.Split(","))
+            .Select(entries => entries.Select(entry =>
+            {
+                var (start, end, _) = entry.Split("-").Select(s => Convert.ToInt32(s)).ToArray();
+                return Enumerable.Range(start, end - start + 1).ToList();
+                }).ToList())
+            .Select(line => (line[0], line[1])).ToList();
+    }
+
+    public override object ExecutePart1() => 
+        Input.Count(tuple => tuple.Item1.All(t => tuple.Item2.Contains(t)) || tuple.Item2.All(t => tuple.Item1.Contains(t)));
+
+    public override object ExecutePart2() => 
+        Input.Count(tuple => tuple.Item1.Any(t => tuple.Item2.Contains(t)) || tuple.Item2.Any(t => tuple.Item1.Contains(t)));
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 My (partial) solutions to advent of code
 
 https://adventofcode.com/
+
+For 2022 I'm focusing on Linq and trying to learn all the ins and outs. Hence some code might not look great as I'm forcing myself to use it even when it does not make much sense. :)
