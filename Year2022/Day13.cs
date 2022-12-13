c361e18fa047b3dfcbe6e4f5a31d4ab4c596d6c6
Lyze <11274700+lyze237@users.noreply.github.com>
2022-12-13 10:15:45 +0100

    Implemented 2022-13

---
+++ Year2022/Day13.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json.Nodes;
+using AdventOfCode.Year2022.Extensions;
+using Tidy.AdventOfCode;
+
+namespace AdventOfCode.Year2022;
+
+public class Day13 : Day
+{
+    public override object ExecutePart1()
+    {
+        var pairs = new List<(JsonNode left, JsonNode right)>();
+
+        foreach (var stringPairs in Input.Split("\n\n"))
+        {
+            var (leftString, rightString, _) = stringPairs.Split("\n");
+            pairs.Add((JsonNode.Parse(leftString!)!, JsonNode.Parse(rightString!))!);
+        }
+
+        return pairs.Select((pair, i) => CompareNodes(pair.left, pair.right) == true ? i + 1 : 0).Sum();
+    }
+
+    public override object ExecutePart2()
+    {
+        var nodes = Input.Split("\n").Where(i => !string.IsNullOrEmpty(i)).Select(i => JsonNode.Parse(i)!).ToList();
+
+        var (first, second) = (JsonNode.Parse("[[2]]")!, JsonNode.Parse("[[6]]")!);
+        nodes.AddRange(new[] { first, second });
+
+        nodes.Sort((left, right) => CompareNodes(left, right) == true ? -1 : 1);
+
+        return (nodes.IndexOf(first) + 1) * (nodes.IndexOf(second) + 1);
+    }
+
+    private static bool? CompareNodes(JsonNode leftNode, JsonNode rightNode)
+    {
+        if (leftNode is JsonValue leftValue && rightNode is JsonValue rightValue)
+            return leftValue.GetValue<int>() == rightValue.GetValue<int>() ? null : leftValue.GetValue<int>() < rightValue.GetValue<int>();
+
+        if (leftNode is not JsonArray leftArray)
+            leftArray = new JsonArray(leftNode.GetValue<int>());
+
+        if (rightNode is not JsonArray rightArray)
+            rightArray = new JsonArray(rightNode.GetValue<int>());
+
+        for (var i = 0; i < leftArray.Count && i < rightArray.Count; i++)
+        {
+            var result = CompareNodes(leftArray[i]!, rightArray[i]!);
+
+            if (result.HasValue)
+                return result.Value;
+        }
+
+        return (leftArray.Count - rightArray.Count) switch
+        {
+            < 0 => true,
+            > 0 => false,
+            _ => null,
+        };
+    }
+}
