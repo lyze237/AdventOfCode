014cba2c9201d7d68325929b809810f498e4db9a
Lyze <11274700+lyze237@users.noreply.github.com>
2023-12-02 14:44:59 +0100

    Implemented 2023-02

---
+++ AoC.2023/Day2.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+using AoC.Framework;
+using AoC.Framework.Extensions;
+
+namespace AoC._2023;
+
+public class Day2 : Day<Day2.Game[]>
+{
+    public record Dice(int Number, string Color);
+
+    public record Game(int Id, Dice[] Picks);
+
+    public Day2() : base(2023, 2)
+    {
+    }
+
+    protected override object DoPart1(Game[] input) => 
+        input.Where(game => game.Picks.All(IsGood)).Sum(game => game.Id);
+
+    private static bool IsGood(Dice dice) =>
+        dice switch
+        {
+            (Number: <= 12, Color: "red") => true,
+            (Number: <= 13, Color: "green") => true,
+            (Number: <= 14, Color: "blue") => true,
+            _ => false
+        };
+
+    protected override object DoPart2(Game[] input) =>
+        input.Sum(game => game.Picks.GroupBy(d => d.Color)
+            .Select(groups => groups.Max(d => d.Number))
+            .Mul());
+
+    protected override Game[] ParseInput(string input) =>
+        (from line in input.Split("\n")
+            let id = Regex.Match(line, @"Game (?<id>\d+):").Groups["id"].Value.ToInt()
+            let rounds = Regex.Matches(line, @"(?<number>\d+) (?<color>\w+)")
+                .Select(die => new Dice(die.Groups["number"].Value.ToInt(), die.Groups["color"].Value))
+                .ToArray()
+            select new Game(id, rounds)).ToArray();
+}
---
+++ AoC.Framework/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace AoC.Framework.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static int Mul(this IEnumerable<int> source) => 
+        source.Aggregate((left, right) => left * right);
+}
