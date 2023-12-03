a897721e0a6daff2eceb24a7cbff593fdf9a2a34
Lyze <11274700+lyze237@users.noreply.github.com>
2023-12-03 09:34:21 +0100

    Updated 2023-03 to Regex

--- AoC.2023/Day3.cs
+++ AoC.2023/Day3.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Framework;
+﻿using System.Text.RegularExpressions;
+using AoC.Framework;
 using AoC.Framework.Extensions;
 
 namespace AoC._2023;
@@ -6,77 +7,59 @@ namespace AoC._2023;
 public class Day3 : Day<Day3.Part[]>
 {
     public record Point(int Y, int X);
+
     public record Symbol(char Type, Point Point);
+
     public record Part(int Number, Symbol Symbol);
-    
-    private readonly (int y, int x)[] directions =
-    {
-        (-1, -1), (1, 0), (1, 1),
-        (0, -1),          (0, 1),
-        (1, -1), (-1, 0), (-1, 1)
-    };
-    
+
     public Day3() : base(2023, 3)
     {
     }
 
-    protected override object DoPart1(Part[] input) => 
+    protected override object DoPart1(Part[] input) =>
         input.Sum(part => part.Number);
 
     protected override object DoPart2(Part[] input) =>
         input
-            .Where(part => part.Symbol.Type == '*') // works without this check too, oversight?
+            .Where(part => part.Symbol.Type == '*') 
             .GroupBy(part => part.Symbol.Point)
             .Where(group => group.Count() == 2)
-            .Sum(group => group.First().Number * group.Last().Number);
+            .Sum(group => group.Select(g => g.Number).Mul());
 
     protected override Part[] ParseInput(string inputString)
     {
-        var input = inputString.Split("\n").Select(line => line.ToCharArray()).ToArray();
-        
+        var input = inputString.Split("\n");
+
         var parts = new List<Part>();
-        for (var y = 0; y < input.Length; y++)
+        for (var i = 0; i < input.Length; i++)
         {
-            var currentNumber = "";
-            Symbol? symbol = null;
-            for (var x = 0; x < input[y].Length; x++)
+            var matches = Regex.Matches(input[i], @"(\d+)");
+            foreach (Match match in matches)
             {
-                var c = input[y][x];
-
-                if (char.IsDigit(c))
-                {
-                    currentNumber += c;
-                    symbol ??= IsAdjacentToSymbol(input, y, x);
-                }
-                else if (!string.IsNullOrWhiteSpace(currentNumber))
-                {
-                    if (symbol != null)
-                        parts.Add(new Part(currentNumber.ToInt(), symbol));
-
-                    currentNumber = "";
-                    symbol = null;
-                }
+                var symbol = IsAdjacentToSymbol(match, i, input);
+                if (symbol != null)
+                    parts.Add(new Part(match.Value.ToInt(), symbol));
             }
-
-            if (!string.IsNullOrWhiteSpace(currentNumber) && symbol != null)
-                parts.Add(new Part(currentNumber.ToInt(), symbol));
         }
-        
+
         return parts.ToArray();
     }
-    
-    private Symbol? IsAdjacentToSymbol(char[][] input, int charY, int charX)
-    {
-        foreach (var (symbolY, symbolX) in directions)
-        {
-            var targetY = charY + symbolY;
-            var targetX = charX + symbolX;
 
-            if (targetY < 0 || targetX < 0 || targetY >= input.Length || targetX >= input[targetY].Length) 
-                continue;
-            
-            if (!char.IsDigit(input[targetY][targetX]) && input[targetY][targetX] != '.')
-                return new Symbol(input[targetY][targetX], new Point(targetY, targetX));
+    private static Symbol? IsAdjacentToSymbol(Match match, int i, string[] input)
+    {
+        for (var x = match.Index - 1; x <= match.Index + match.Length; x++)
+        {
+            for (var y = i - 1; y <= i + 1; y++)
+            {
+                if (x < 0 || y < 0 || y >= input.Length || x >= input[i].Length)
+                    continue;
+
+                var c = input[y][x];
+                if (char.IsDigit(c) || c == '.')
+                    continue;
+
+                return new Symbol(c, new Point(y, x));
+            }
         }
 
         return null;
