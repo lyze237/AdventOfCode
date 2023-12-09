43e515eff5124e8ad603bbe48dcc2880576f754b
Lyze <11274700+lyze237@users.noreply.github.com>
2023-12-09 10:49:36 +0100

    Implemented 2023-09

---
+++ AoC.2023/Day9.cs
@@ -0,0 +1,31 @@
+﻿using AoC.Framework;
+using AoC.Framework.Extensions;
+using NUnit.Framework;
+
+namespace AoC._2023;
+
+[TestFixture]
+public class Day9 : Day<long[][]>
+{
+    public Day9() : base(2023, 9, true)
+    {
+    }
+
+    protected override object DoPart1(long[][] input) =>
+        input.Select(Solve).Sum();
+    
+    protected override object DoPart2(long[][] input) =>
+        input.Select(i => i.Reverse().ToArray()).Select(Solve).Sum();
+
+    private static long Solve(long[] line) => 
+        !line.Any() ? 0 : Solve(FindNextValues(line)) + line.Last();
+
+    private static long[] FindNextValues(long[] line) => 
+        line.Zip(line.Skip(1)).Select(l => l.Second - l.First).ToArray();
+
+    protected override long[][] ParseInput(string input) => 
+        input
+            .Split("\n")
+            .Select(line => line.Split(" ").Select(i => i.ToLong()).ToArray())
+            .ToArray();
+}
