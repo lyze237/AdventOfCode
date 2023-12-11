f0a0ad393ab47d9390dd55ce19c338d830a1b184
Lyze <11274700+lyze237@users.noreply.github.com>
2023-12-11 14:10:11 +0100

    Improved Day1

--- AoC.2023/Day1.cs
+++ AoC.2023/Day1.cs
@@ -7,6 +7,8 @@ namespace AoC._2023;
 [TestFixture]
 public class Day1 : Day
 {
+    private static readonly Dictionary<string, int> Numbers = new() { {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9} };
+
     public Day1() : base(2023, 1, true)
     {
     }
@@ -15,24 +17,13 @@ public class Day1 : Day
         Run(input, @"\d");
 
     protected override object DoPart2(string[] input) =>
-        Run(input, @"one|two|three|four|five|six|seven|eight|nine|\d");
+        Run(input, @$"{string.Join("|", Numbers.Select(n => n.Key))}|\d");
 
     private static int Run(IEnumerable<string> input, string regex) =>
         input.Select(line =>
             ConvertMatch(Regex.Match(line, regex).Value) * 10 +
             ConvertMatch(Regex.Match(line, regex, RegexOptions.RightToLeft).Value)).Sum();
 
-    private static int ConvertMatch(string str) => str switch
-    {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        _ => Convert.ToInt32(str)
-    };
+    private static int ConvertMatch(string str) => 
+        int.TryParse(str, out var number) ? number : Numbers[str];
 }
